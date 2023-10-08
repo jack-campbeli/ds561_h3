87541d913e4465ec5c66774e96d3a682b84deda9
Jack Campbell <jackwcampbell11@gmail.com>
2023-10-08 19:35:15 -0400

    start app2 (not working)

--- app1.py
+++ app1.py
@@ -1,6 +1,9 @@
 import functions_framework
 from flask import Request, Response
 from google.cloud import storage
+from google.cloud import pubsub_v1
+
+banned = ['north korea', 'iran', 'cuba', 'myanmar', 'iraq', 'libya', 'sudan', 'zimbabwe', 'syria']
 
 def get_file_name(request: Request):
     url_path = request.path
@@ -23,26 +26,45 @@ def files_get(request):
         print("Method not implemented: ", method)
         return 'Not Implemented', 501
     else:
-        file_name = get_file_name(request)
 
-        client = storage.Client()
+        country = request.headers.get('X-country')
+        print("Country: ", country.lower())
         
-        bucket_name = 'bu-ds561-jawicamp'
-        bucket = client.bucket(bucket_name)
-        
-        blob = bucket.blob(file_name)
-        
-        try:
+        if country.lower() not in banned:
 
-            content = blob.download_as_text()
+            file_name = get_file_name(request)
 
-            response = Response(content, status=200, headers={'Content-Type': 'text/html'})
+            client = storage.Client()
+            
+            bucket_name = 'bu-ds561-jawicamp'
+            bucket = client.bucket(bucket_name)
+            
+            blob = bucket.blob(file_name)
 
-            return response
+            try:
 
-        except Exception as e:
-            print("File not found: ",file_name)
-            return str(e), 404  
+                content = blob.download_as_text()
+
+                response = Response(content, status=200, headers={'Content-Type': 'text/html'})
+
+                return response
+
+            except Exception as e:
+                print("File not found: ",file_name)
+                return str(e), 404  
+            
+        else:
+            print("Permission Denied")
+            publisher = pubsub_v1.PublisherClient()
+            topic_path = publisher.topic_path('jacks-project-398813', 'banned_countries-sub')
+    
+            data_str = f"{country}"
+            # Data must be a bytestring
+            data = data_str.encode("utf-8")
+            # When you publish a message, the client returns a future.
+            future = publisher.publish(topic_path, data)
+            print(future.result())
+            return "Permission Denied", 400
 
 # Notes:
 # - python http-client.py -d "us-central1-jacks-project-398813.cloudfunctions.net" -b "none" -w "files_get" -v -n 5 -i 10000 

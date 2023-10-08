923a7990868895a07a946fbca08e58f98e9e458a
Jack Campbell <jackwcampbell11@gmail.com>
2023-10-08 14:27:05 -0400

    Final Q1 Commit

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ from google.cloud import storage
 
 def get_file_name(request: Request):
     url_path = request.path
-    # Split the URL path using '/' as a separator and get the last element
+    # split url path with '/' and get the last element
     file_name = url_path.split('/')[-1]
     
     return file_name
@@ -18,17 +18,20 @@ def files_get(request):
     Returns:
        
     """
-    # Get the HTTP method (GET, POST, etc.)
     method = request.method
     if method != 'GET':
+        print("Method not implemented: ", method)
         return 'Not Implemented', 501
     else:
         file_name = get_file_name(request)
 
         client = storage.Client()
+        
         bucket_name = 'bu-ds561-jawicamp'
         bucket = client.bucket(bucket_name)
+        
         blob = bucket.blob(file_name)
+        
         try:
 
             content = blob.download_as_text()
@@ -38,7 +41,8 @@ def files_get(request):
             return response
 
         except Exception as e:
-            return str(e), 404  # Return a 404 Not Found response if the file is not found or an error occurs
+            print("File not found: ",file_name)
+            return str(e), 404  
 
 # Notes:
 # - python http-client.py -d "us-central1-jacks-project-398813.cloudfunctions.net" -b "none" -w "files_get" -v -n 5 -i 10000 
